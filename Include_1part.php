6430c49f3f9f68bd7208de7eb0f258225a0d07b7
Ansh Singh <101971905+anshsingh7@users.noreply.github.com>
2022-06-04 02:43:10 +0530

    Add files via upload

---
+++ Include_1part.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Control to another page</title>
+</head>
+
+<body>
+    <h1 style="border:1px solid red;">Include</h1>
+    <h3> Ansh Singh </h3>;
+    <div class="footer">
+        <h1><a href="Include_2part.php">This is the Footer.<a></h1>
+    </div>
+</body>
+
+</html>
---
+++ Include_2part.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Control to another page</title>
+</head>
+
+<body>
+
+    <?php include 'Include_1part.php'; ?>
+
+</body>
+
+</html>
+
+
