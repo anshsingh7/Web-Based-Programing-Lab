2b97b0a38af6c4c931a3bf30c24a2d1a08197a13
Ansh Singh <101971905+anshsingh7@users.noreply.github.com>
2022-06-04 09:05:36 +0530

    Add files via upload

---
+++ Exit_Die_function.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Exit and Die Function</title>
+</head>
+
+<body bgcolor="powderblue">
+    <?php
+    echo "<h1> Exit and Die Function </h1>";
+    echo "<h2> Ansh Singh </h2>";
+    echo "<h1>Hi</h1>";
+   
+    echo "<h1>Hello</h1>";
+
+    exit("<h1>page not found</h1>"); //Here, you can use die too, because die is an alias of exit()
+   
+    echo "<h1>Hey</h1>";
+    ?>
+</body>
+
+</html>
