da34608da47bc7d3d2da68404683f275bf21e83f
Ansh Singh <101971905+anshsingh7@users.noreply.github.com>
2022-06-09 22:24:55 +0530

    Add files via upload

---
+++ Arithmetic_Operators.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <TITLE>PHP Operators</TITLE>
+</head>
+
+<body bgcolor="powderblue">
+    <?php
+    echo "<h1> Operators </h1>";
+    echo "<h2> Arithmetic Operators :- </h2>";
+    echo "<h3> Ansh Singh </h3>";
+
+    $a = 10;
+    $b = 6;
+    echo "Variable1 is $a <br>";
+    echo "Variable2 is $b <br>";
+
+    echo "Addition operation = " . ($a + $b), "<br>";
+    echo "Subtrction operation = " . ($a - $b), "<br>";
+    echo "Multiplication operation = " . ($a * $b), "<br>";
+    echo "Division operation = " . ($a / $b), "<br>";
+    echo "Modulus operation = " . ($a % $b), "<br>";
+    ?>
+</body>
+
+</html>
